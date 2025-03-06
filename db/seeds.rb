80e2a80c48716e4d78fdd2c65507b0d3dfaa7d51
Mikael Renvoise <klimae@icloud.com>
2025-03-06 12:50:37 +0100

    validates ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+Movie.create(title: , overview: , poster_url: , rating: )
