1923b7b3d497c38a5fda4deb2a2d417abf3410b1
Mikael Renvoise <klimae@icloud.com>
2025-03-07 15:55:45 +0100

    Watch list, front a faire

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,16 +7,19 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to @list, notice: "Film ajouté à la liste !"
+      redirect_to list_path(@bookmark.list), notice: "Film ajouté à la liste !"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create destroy]
+    resources :bookmarks, only: %i[new create]
+  resources :bookmarks, only: :destroy
   end
 end
